e5fe5de67e956164a91b6ace68def523ebf9982f
Qinbeans <rybeats@live.com>
2017-04-06 20:41:41 -0700

    FIX
    Needs to be fixed.  Look into play class for more information.  You will need to look at both buttons and play.

--- Buttons.java
+++ Buttons.java
@@ -1,65 +1,51 @@
-import javax.swing.JPanel;
-import javax.swing.JButton;
-import javax.swing.ImageIcon;
-import java.io.IOException;
-import java.net.URL;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.UnsupportedAudioFileException;
-import javax.sound.sampled.LineUnavailableException;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
 import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.io.IOException;
 import java.util.Map;
+import java.awt.Color;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.net.URL;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.sound.sampled.LineUnavailableException;
 import java.util.HashMap;
-import java.awt.image.BufferedImage;
 /**
- * !!Will not disclose name since jar files can be accessed too easily!!
- * Purpose: Be a piano of piece and calmness.
- * Here: class for buttons in the menu.
+ * Write a description of class Buttons here.
  * 
- * @author QinBeans(Chin beans;RF)
- * @version 1.9
+ * @author (your name) 
+ * @version (a version number or a date)
  */
-public class Buttons extends JPanel
+public class Buttons
 {
     private static String[] keycount = {"A0","Bb0","B0"
-        ,"C1","Db1","D1","Eb1","E1","F1","Gb1","G1","Ab1","A1","Bb1","B1"
-        ,"C2","Db2","D2","Eb2","E2","F2","Gb2","G2","Ab2","A2","Bb2","B2"
-        ,"C3","Db3","D3","Eb3","E3","F3","Gb3","G3","Ab3","A3","Bb3","B3"
-        ,"C4","Db4","D4","Eb4","E4","F4","Gb4","G4","Ab4","A4","Bb4","B4"
-        ,"C5","Db5","D5","Eb5","E5","F5","Gb5","G5","Ab5","A5","Bb5","B5"
-        ,"C6","Db6","D6","Eb6","E6","F6","Gb6","G6","Ab6","A6","Bb6","B6"
-        ,"C7","Db7","D7","Eb7","E7","F7","Gb7","G7","Ab7","A7","Bb7","B7"
-        ,"C8",};
-    public static void enable(JPanel panel){
-        //Cut for ease of coding (Section 7)
-        String in = "";
-        BufferedImage squareImg = new BufferedImage(5, 25, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2 = squareImg.createGraphics();
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        int x=0;
-        int y=0;
+            ,"C1","Db1","D1","Eb1","E1","F1","Gb1","G1","Ab1","A1","Bb1","B1"
+            ,"C2","Db2","D2","Eb2","E2","F2","Gb2","G2","Ab2","A2","Bb2","B2"
+            ,"C3","Db3","D3","Eb3","E3","F3","Gb3","G3","Ab3","A3","Bb3","B3"
+            ,"C4","Db4","D4","Eb4","E4","F4","Gb4","G4","Ab4","A4","Bb4","B4"
+            ,"C5","Db5","D5","Eb5","E5","F5","Gb5","G5","Ab5","A5","Bb5","B5"
+            ,"C6","Db6","D6","Eb6","E6","F6","Gb6","G6","Ab6","A6","Bb6","B6"
+            ,"C7","Db7","D7","Eb7","E7","F7","Gb7","G7","Ab7","A7","Bb7","B7"
+            ,"C8",};
+    public static void enable(JPanel panel,String name){
         for(String comb:keycount){
             Map<String,JButton> attempt = new HashMap<String,JButton>();
-            attempt.put(comb,new JButton());
+            attempt.put(comb,new JButton(comb));
             JButton holder = attempt.get(comb);
-            if(comb.contains((CharSequence)"b")){
-                g2.setColor(Color.BLACK);
-                g2.fillRect(x, y, 5, 25);
-                g2.dispose();
-                holder.setIcon(new ImageIcon(squareImg));
+            if(name==comb){
+                holder.setBackground(Color.YELLOW);
+                holder.setForeground(Color.RED);
+                holder.setOpaque(true);
+            }else if(comb.contains((CharSequence)"b")){
                 holder.setBackground(Color.BLACK);
+                holder.setForeground(Color.RED);
                 holder.setOpaque(true);
             }else{
-                g2.setColor(Color.WHITE);
-                g2.fillRect(x, y, 5, 50);
-                g2.dispose();
-                holder.setIcon(new ImageIcon(squareImg));
                 holder.setBackground(Color.WHITE);
+                holder.setForeground(Color.RED);
                 holder.setOpaque(true);
             }
             holder.addActionListener(new ActionListener(){
@@ -80,11 +66,17 @@ public class Buttons extends JPanel
                         } catch (LineUnavailableException e) {
                             e.printStackTrace();
                         }
-                        //Chords(char for keysignature,int for which key,char for value of character for keybind)
                     }
                 });
             panel.add(holder);
         }
-
+    }
+    public static void disable(JPanel panel){
+        for(String comb:keycount){
+            Map<String,JButton> attempt = new HashMap<String,JButton>();
+            attempt.put(comb,new JButton());
+            JButton holder = attempt.get(comb);
+            panel.remove(holder);
+        }
     }
 }
